1fc57a73a6c7d13b88cfb72c090fc8a9c7b532c9
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-12-23 14:22:09 +0900

    記事一覧表示機能追加

--- katachi-blog-spring4/src/main/java/com/katachi/blog/repository/PostRepository.java
+++ katachi-blog-spring4/src/main/java/com/katachi/blog/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.katachi.blog.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.katachi.blog.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Integer>{
 
+	@Query("select p from Post p join fetch p.category")
+	List<Post> findAll();
 }
---
+++ katachi-blog-spring4/src/main/java/com/katachi/blog/service/PostService.java
@@ -0,0 +1,10 @@
+package com.katachi.blog.service;
+
+import java.util.List;
+
+import com.katachi.blog.model.Post;
+
+public interface PostService {
+
+	List<Post> getPosts();
+}
