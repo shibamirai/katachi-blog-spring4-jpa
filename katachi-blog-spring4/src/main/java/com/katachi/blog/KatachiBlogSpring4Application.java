d68c66b086d5605e2640d99852b10fcf7989dd3e
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-12-23 10:07:09 +0900

    以下の構成でプロジェクト作成
    Spring Web
    
    Thymeleaf
    
    H2 Database
    
    Spring Data JPA
    
    Validation
    
    Spring Security
    
    Spring Boot DevTools
    
    Lombok
    
    追加の依存関係
    
    Thymeleaf-layout-dialect
    
    Webjars-locator-lite
    
    Bootstrap
    
    Popper JS
    
    Spring AOP

---
+++ katachi-blog-spring4/src/main/java/com/katachi/blog/KatachiBlogSpring4Application.java
@@ -0,0 +1,13 @@
+package com.katachi.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatachiBlogSpring4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatachiBlogSpring4Application.class, args);
+	}
+
+}
