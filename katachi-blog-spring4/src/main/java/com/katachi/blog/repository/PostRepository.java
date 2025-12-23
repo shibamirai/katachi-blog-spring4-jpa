c01a911054f3a5e81752293775415c30c3a9454b
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-12-23 15:59:37 +0900

    ページネーションや検索に対応できるように Join fetch を JPQL でなく Specification で行うよう変更

--- katachi-blog-spring4/src/main/java/com/katachi/blog/repository/PostRepository.java
+++ katachi-blog-spring4/src/main/java/com/katachi/blog/repository/PostRepository.java
@@ -1,14 +1,10 @@
 package com.katachi.blog.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.katachi.blog.model.Post;
 
-public interface PostRepository extends JpaRepository<Post, Integer>{
+public interface PostRepository extends JpaRepository<Post, Integer>, JpaSpecificationExecutor<Post> {
 
-	@Query("select p from Post p join fetch p.category")
-	List<Post> findAll();
 }
