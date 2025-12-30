570a496d252ce9fee87a6e4a8a300d66f912b391
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-12-30 12:17:52 +0900

    コメント表示機能追加

--- katachi-blog-spring4/src/main/java/com/katachi/blog/controller/PostController.java
+++ katachi-blog-spring4/src/main/java/com/katachi/blog/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.katachi.blog.controller;
 
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.katachi.blog.exception.ResourceNotFoundException;
+import com.katachi.blog.model.Comment;
 import com.katachi.blog.model.Post;
+import com.katachi.blog.service.CommentService;
 import com.katachi.blog.service.PostService;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
+
+	@Autowired
+	private CommentService commentService;
 
 	@Autowired
 	private PostService postService;
@@ -40,6 +46,10 @@ public class PostController {
 
 		Post post = postService.getPostBySlug(slug);
 		model.addAttribute(post);
+
+		// 記事とコメントは別々に取得する
+		List<Comment> comments = commentService.getByPostId(post.getId());
+		model.addAttribute("comments", comments);
 
 		return "posts/show";
 	}
