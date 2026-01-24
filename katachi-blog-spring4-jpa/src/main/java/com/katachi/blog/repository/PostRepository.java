74679ce329e60a27740e27e676a1a2fcd5706959
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-01-24 11:56:10 +0900

    ページネーションや検索に対応できるように Join fetch を JPQL でなく Specification で行うよう変更

--- katachi-blog-spring4-jpa/src/main/java/com/katachi/blog/repository/PostRepository.java
+++ katachi-blog-spring4-jpa/src/main/java/com/katachi/blog/repository/PostRepository.java
@@ -1,15 +1,10 @@
 package com.katachi.blog.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.katachi.blog.model.Post;
 
-public interface PostRepository extends JpaRepository<Post, Integer>{
-
-	@Query("select p from Post p join fetch p.category order by p.postedAt desc")
-	List<Post> findAll();
+public interface PostRepository extends JpaRepository<Post, Integer>, JpaSpecificationExecutor<Post> {
 
 }
--- katachi-blog-spring4-jpa/src/main/java/com/katachi/blog/service/impl/PostServiceImpl.java
+++ katachi-blog-spring4-jpa/src/main/java/com/katachi/blog/service/impl/PostServiceImpl.java
@@ -2,7 +2,10 @@ package com.katachi.blog.service.impl;
 
 import java.util.List;
 
+import jakarta.persistence.criteria.JoinType;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.katachi.blog.model.Post;
@@ -15,9 +18,19 @@ public class PostServiceImpl implements PostService {
 	@Autowired
 	private PostRepository postRepository;
 
+	/**
+	 * categoryをjoin fetchするSpecification作成
+	 */
+	private Specification<Post> join() {
+		return (root, _, builder) -> {
+			root.fetch("category", JoinType.INNER);
+			return builder.conjunction();
+		};
+	}
+
 	@Override
 	public List<Post> getPosts() {
-		return postRepository.findAll();
+		return postRepository.findAll(join());
 	}
 
 }
